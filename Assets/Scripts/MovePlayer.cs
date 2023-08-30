fc3e8c996b8dde64d07cf766c637ade2e98115b7
damianovisa <82058074+damianovisa@users.noreply.github.com>
2023-08-30 15:20:20 -0400

    Add files via upload

---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    float strength = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        rigidBody.AddForce(new Vector3(-vertical,0,horizontal) * strength);
+    }
+}
